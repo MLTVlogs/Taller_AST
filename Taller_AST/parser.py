8d0a054674ff93d607c28a75ee3610c001ade9c9
Kamaio <jfocampo173@gmail.com>
2026-03-26 16:37:03 -0500

    Todo listo, faltan pruebas unitarias

--- Taller_AST/parser.py
+++ Taller_AST/parser.py
@@ -8,7 +8,8 @@ from typing import List, Any, Optional, Union
 from lexer  import Lexer
 from errors import error, errors_detected
 from model  import *
-from graphviz import *
+from graphviz import Digraph
+import uuid
 
 
 def _L(node, lineno):
@@ -668,29 +669,23 @@ def build_rich_tree(node):
 
 
 # AST a Graphviz
-def ast_to_graphviz(node, graph=None, parent_id=None):
-	#if graph is None:
-	#	graph = Digraph()
-	
-	node_id = str(id(node))
-	label = type(node).__name__
-	graph.node(node_id, label)
-	
-	if parent_id is not None:
-		graph.edge(parent_id, node_id)
-	
-	for key, value in vars(node).items():
-		if isinstance(value, list):
-			for item in value:
-				ast_to_graphviz(item, graph, node_id)
-		elif hasattr(value, "__dict__"):
-			ast_to_graphviz(value, graph, node_id)
-		else:
-			value_id = f"{node_id}_{key}"
-			graph.node(value_id, f"{key}: {value}")
-			graph.edge(node_id, value_id)
-	
-	return graph
+def ast_to_graphviz(node, dot, parent_id=None):
+    node_id = str(uuid.uuid4())
+    label = type(node).__name__
+
+    dot.node(node_id, label)
+
+    if parent_id:
+        dot.edge(parent_id, node_id)
+
+    for field, value in vars(node).items():
+        if isinstance(value, list):
+            for item in value:
+                ast_to_graphviz(item, dot, node_id)
+        elif hasattr(value, "__dict__"):
+            ast_to_graphviz(value, dot, node_id)
+
+    return dot
 
 
 
@@ -717,11 +712,9 @@ if __name__ == '__main__':
 		if not errors_detected():
 
 			if "-graphviz" in sys.argv:
-				graph = ast_to_graphviz(ast)
+				graph = Digraph()
+				ast_to_graphviz(ast, graph)
 				graph.render('ast', format='png', cleanup=True)
 			elif "-rich" in sys.argv:
 				tree = build_rich_tree(ast)
 				print(tree)
-
-		
-		
